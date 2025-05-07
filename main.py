a2286138c9d1cfb0d5656be47c28255797fd7bbf
pessina11 <l285712@dac.unicamp.br>
2025-05-07 20:05:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from bandeira import identificar_bandeira
+
+if __name__ == "__main__":
+    numero = input("Digite o número do cartão: ")
+    print("Bandeira detectada:", identificar_bandeira(numero))
 
